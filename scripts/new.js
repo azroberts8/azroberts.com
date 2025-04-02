ee3cd5a673a97805b39f029545d9743c7427f5fe
Andrew Roberts <andrew@azroberts.com>
2025-04-02 10:21:02 -0400

    Completed navbar

--- scripts/new.js
+++ scripts/new.js
@@ -1,9 +1,11 @@
 function toggle(x) {
     x.classList.toggle("x");
     const nav = document.querySelector('nav')
-    if(nav.style.height === nav.scrollHeight + 'px') {
-        nav.style.height = '0px';
-    } else {
+    if(nav.style.height === '0px') {
         nav.style.height = nav.scrollHeight + 'px';
+        nav.style.marginBottom = "var(--size-0)";
+    } else {
+        nav.style.height = '0px';
+        nav.style.marginBottom = "0px";
     }
 }
